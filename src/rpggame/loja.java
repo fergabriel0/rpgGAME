fa23ec60ec9e52b25fdf8e125a6e0c6a8afb9110
fergabriel0 <fernandogabMG@gmail.com>
2023-12-26 10:27:41 -0300

    pm4 loja

--- src/rpggame/loja.java
+++ src/rpggame/loja.java
@@ -8,6 +8,8 @@ public class loja {
     private String nome;
     private String descricao;
     
+    public loja() {}
+    
     public loja(int preco, String nome, String descricao, int precoVENDA) {
         preco = this.preco;
         nome = this.nome;
@@ -17,13 +19,16 @@ public class loja {
     
     /* arrays de objetos a serem vendidos */
     ArrayList<loja> lista1 = new ArrayList<loja>();
-    loja item1 = new loja(10, "poção de cura", "cura o usuário em 10 pontos.",5);
-    loja item2 = new loja(20, "poção de mana", "cura a mana do usuário em 50 pontos.",10);
-    loja item3 = new loja(15, "poção de defesa", "concede 4 de defesa por 1 batalha.",8);
+
+    public ArrayList getLista1() {
+        loja item1 = new loja(10, "poção de cura", "cura o usuário em 10 pontos.",5);
+        loja item2 = new loja(20, "poção de mana", "cura a mana do usuário em 50 pontos.",10);
+        loja item3 = new loja(15, "poção de defesa", "concede 4 de defesa por 1 batalha.",8);
     
-    ArrayList<loja> lista2 = new ArrayList<loja>();
-    loja item4 = new loja(30, "poção de ataque", "concede 4 de ataque por 1 batalha.",15);
-    loja item5 = new loja(40, "amuleto da sorte", "enche o usuário com fortuna e sorte.",20);
-    loja item6 = new loja(50, "espada enferrujada", "quando equipada concede +2 em cada ataque.",25);
-    
+        lista1.add(item1);
+        lista1.add(item2);
+        lista1.add(item3);
+        
+        return lista1;
+    }
 }
